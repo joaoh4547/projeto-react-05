67031487da511930f4eeca393e4ce50b1b2e19a6
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-02 15:48:33 -0300

    Capitulo 2 Parte 6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
+import "keen-slider/keen-slider.min.css";
 import Head from "next/head";
 import { HomeContainer, Product } from "../styles/pages/home";
 import Image, { StaticImageData } from "next/future/image";
 import image1 from "../assets/camisetas/1.png";
 import image2 from "../assets/camisetas/2.png";
-// import image3 from "../assets/camisetas/3.png";
-// import image4 from "../assets/camisetas/4.png";
+import image3 from "../assets/camisetas/3.png";
+import image4 from "../assets/camisetas/4.png";
+
+import { useKeenSlider } from "keen-slider/react";
 
 
 interface Product{
@@ -21,28 +24,34 @@ const camisetas: Record<number, Product> = {
         image: image2,
         price: 24.99,
     },
-    // 3: {
-    //     image: image3,
-    //     price: 17.99,
-    // },
-    // 4: {
-    //     image: image4,
-    //     price: 22.99,
-    // },
+    3: {
+        image: image3,
+        price: 17.99,
+    },
+    4: {
+        image: image4,
+        price: 22.99,
+    },
 };
 
 console.log();
 
 export default function Home() {
+
+    const [sliderRef]  = useKeenSlider({slides: {
+        perView: 2.25,
+        spacing: 48,
+        
+    }});
     return (
         <>
             <Head>
                 <title>Ignite Shop</title>
             </Head>
-            <HomeContainer>
+            <HomeContainer ref={sliderRef} className="keen-slider">
                 {Array.from({length: Object.entries(camisetas).length}).map((_, i) =>{
                     return (
-                        <Product key={i} href={`product/${i+1}`}>
+                        <Product key={i} href={`product/${i+1}`} className="keen-slider__slide">
                             <Image src={camisetas[i+1].image} alt={`camiseta-${i+1}`} width={520} height={480}/>
                             <footer>
                                 <strong>Camiseta {i+1}</strong>
