7a2843b187dd10ac7b7e1ffc395877539f1461df
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-02 12:06:51 -0300

    Capitulo 2 Parte 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,29 @@
+import { styled } from "../styles";
+
+const Button = styled("button",{
+    display: "flex",
+    backgroundColor: "$color",
+    borderRadius: 8,
+    border: 0,
+    padding: "4px 8px",
+    span:{
+        fontWeight: "bold"
+    },
+    transition: "0.2s",
+    gap: 5,
+    "&:hover":{
+        filter: "brightness(0.8)"
+    }
+
+});
 
 export default function Home() {
     return (
-        <h1>
-          Hello World
-        </h1>
+
+        <Button>
+            Enviar 
+            <span>teste</span>
+        </Button>
+    
     );
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,9 @@
+import { createStitches } from "@stitches/react";
+
+export const {config,styled,globalCss, keyframes, theme,createTheme, css, getCssText } = createStitches({
+    theme: {
+        colors:{
+            color: "#8257e6"
+        }
+    }
+});
