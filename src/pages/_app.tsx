dd2469d7eddc47e53b198fba19a4adfed4a1379a
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-02 13:49:15 -0300

    Capitulo 2 Parte 3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
+import logoImage from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
 
 globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
     
-    return  <Component {...pageProps} />;
+    return (
+        <Container>
+            <Header>
+                <img {...logoImage}  alt="logo"/>
+            </Header>
+            <Component {...pageProps} />
+        </Container>
+    );
 }
