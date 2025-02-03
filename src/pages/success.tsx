4d71eaac8e51aae30255f9dce83da5d0bb841dba
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-02 21:49:34 -0300

    Capitulo 4 Parte 12

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -30,21 +30,24 @@ export default function Success({customerName,product}:SuccessProps){
     );
 }
 
-export const getServerSideProps : GetServerSideProps = async ({query,params}) =>{
+export const getServerSideProps : GetServerSideProps = async ({query}) =>{
+    if(!query.session_id){
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false
+            }
+        };
+    }
+    
     const sessionId =  String(query.session_id);
+
+    
     
     const session = await stripe.checkout.sessions.retrieve(sessionId,{ expand: ["line_items","line_items.data.price.product"]});
 
-    // session.line_items
-    console.log(session);
-    console.log(session.line_items.data[0].price.product);
-
     const product  = session.line_items.data[0].price.product as Stripe.Product;
 
-    const productDetails = {
-        
-       
-    };
     return {
         props: {
             customerName: session.customer_details.name,
